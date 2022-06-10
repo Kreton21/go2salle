efd12d10b6cde54fe4d19c6557bd4480376f7987
Kreton21 <dragulinescualexandru@tuta.io>
2022-06-10 12:17:15 +0200

    faut tt reparer

--- bolted/bolted.go
+++ bolted/bolted.go
@@ -11,6 +11,7 @@ import (
 func InitDay(salles int) error {
 	day := time.Now()
 	date := day.Format("2006-01-02")
+
 	fmt.Println(date)
 	db, err := bolt.Open("reserv.db", 0600, nil)
 	if err != nil {
@@ -36,8 +37,58 @@ func InitDay(salles int) error {
 	return nil
 
 }
-func Reserv(date, time, salle, id) {
+func Reserv(datenow, date, time, salle, id string) error {
+	db, err := bolt.Open("reserv.db", 0600, nil)
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer db.Close()
+	keyval := date + time + salle + id
 
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(datenow))
+		c := b.Bucket([]byte(salle))
+		var keye int
+		if err := b.ForEach(func(k, v []byte) error {
+			if err != nil {
+				fmt.Println(err)
+				k = []byte("1")
+			}
+			keye, err = strconv.Atoi(string(k))
+			if err != nil {
+				// handle error
+				return err
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+		key := []byte(strconv.Itoa(keye))
+		err := c.Put(key, []byte(keyval))
+		return err
+	})
+	fmt.Println(keyval)
+	return nil
+}
+func Test(date, salle string) {
+	db, err := bolt.Open("reserv.db", 0600, nil)
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(date))
+		c := b.Bucket([]byte(salle))
+		err = c.ForEach(func(k, v []byte) error {
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("La cle", string(k), "et la valeur", string(v))
+			return err
+		})
+		return err
+	})
 }
 
 func Wdb(bucket *bolt.Bucket, key, value []byte) {
--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	// "go2salle/worker"
 
+	"fmt"
 	"go2salle/bolted"
 	"go2salle/webapp"
+	"time"
 	// "github.com/gorilla/mux"
 )
 
 func main() {
 	webapp.Main()
+	day := time.Now()
+	date := day.Format("2006-01-02")
+	bolted.InitDay(5)
+	bolted.Reserv(date, "22-09-2005", "12-21", "2", "Kreton")
+
+	fmt.Println("reserved")
+	bolted.Test(date, "2")
 	// bolted.Main()
 	bolted.InitDay(5)
 }
